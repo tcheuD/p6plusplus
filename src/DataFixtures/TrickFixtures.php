b11ba140c3a7ae6d87e3d05c84d27eb8bcfb82a3
tcheuD <tcheutcheud@gmail.com>
2019-02-06 11:48:12 +0100

    Created Video and Pictures fixtures

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -8,11 +8,25 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class TrickFixtures extends BaseFixture implements DependentFixtureInterface
 {
+    private static $titles = [
+        'Ollie',
+        'Nollie',
+        'Switch ollie',
+        'One-Two',
+        'A B',
+        'Bloody Dracula',
+        'Canadian Bacon',
+        'Chicken Salad',
+        'China air',
+        'Crail',
+        'Cross-rocket',
+    ];
+
     protected function loadData(ObjectManager $manager)
     {
         $this->createMany(10, 'trick', function ($i) {
             $trick = new Trick();
-            $trick->setTitle($this->faker->firstName)
+            $trick->setTitle($this->faker->randomElement(self::$titles))
                 ->setCreationDate($this->faker->dateTimeBetween('-100 days', '-1 days'))
                 ->setCategory('snow')
                 ->setCreatedBy($this->getRandomReference('user'))
