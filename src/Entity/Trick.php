b7644e9a921d4ed003724e01b2ca3dcb8f02b758
tcheuD <tcheutcheud@gmail.com>
2019-02-11 16:38:18 +0100

    Title field must be unique

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -5,9 +5,11 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
+ * @UniqueEntity("title")
  */
 class Trick
 {
@@ -19,7 +21,7 @@ class Trick
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=100)
+     * @ORM\Column(name="title", type="string", length=100, unique=true)
      */
     private $title;
 
