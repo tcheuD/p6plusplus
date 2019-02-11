2c3ca8b06a63ec0ccd12896504f6c95a0a482610
tcheuD <tcheutcheud@gmail.com>
2019-02-11 16:36:39 +0100

    Replaced "username" field by "firstname" and "nickname" Added a new field for the user's picture Updated user fixtures

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -7,15 +7,31 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class UserFixture extends BaseFixture
 {
+    private static $picture = [
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+        '4.jpg',
+        '5.jpg',
+        '6.jpg',
+        '7.jpg',
+        '8.jpg',
+        '9.jpg',
+        '10.jpg',
+        '11.jpg',
+    ];
+
     public function loadData(ObjectManager $manager)
     {
         $this->createMany(10, 'user', function ($i) {
            $user = new User();
            $user->setEmail(sprintf('snowtrick@snowtrick.com', $i));
-           $user->setUsername($this->faker->name());
+           $user->setFirstname($this->faker->firstName());
+           $user->setName($this->faker->name());
            $user->setPassword($this->faker->password());
            $user->setForgotPassIdentity($this->faker->password());
            $user->setRegistrationDate($this->faker->dateTimeBetween('-100 days', '-1 days'));
+           $user->setPicture($this->faker->randomElement(self::$picture));
 
            return $user;
         });
