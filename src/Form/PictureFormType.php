e1f9008e48dbc3c0037084a7cc345eb16e80c9cb
tcheuD <tcheutcheud@gmail.com>
2019-02-21 10:32:20 +0100

    Added PictureFormType.php

---
+++ src/Form/PictureFormType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Picture;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PictureFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('image', FileType::class, ['label' => 'Image'])
+        ;
+    }
+}
