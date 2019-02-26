ebc716486c99b013a6a2eeba50301de846272bb5
tcheuD <tcheutcheud@gmail.com>
2019-02-26 19:16:57 +0100

    Fix url duplication

--- src/DataFixtures/VideoFixture.php
+++ src/DataFixtures/VideoFixture.php
@@ -20,12 +20,32 @@ class VideoFixture extends BaseFixture implements DependentFixtureInterface
         'https://youtu.be/SQyTWk7OxSI',
         'https://youtu.be/rB9lOfv0oPQ',
         'https://youtu.be/74UEgGrawKA',
+        'https://youtu.be/K-apShaeSgY',
+        'https://youtu.be/Xs-JfPjgiA4',
+        'https://youtu.be/de50trNEvf8',
+        'https://youtu.be/WNW90gFK9Cs',
+        'https://youtu.be/0AbiygSo478',
+        'https://www.youtube.com/watch?v=n0F6hSpxaFc',
+        'https://youtu.be/UGdif-dwu-8',
+        'https://youtu.be/FNyCrnsMlDE',
+        'https://youtu.be/AI1m8NF7sp4',
+        'https://youtu.be/1z4LMq8d7DI',
+        'https://youtu.be/LRfk90XHqyY',
+        'https://youtu.be/SDP6eGGYqYA',
+        'https://youtu.be/RY8CBQZEZ34',
+        'https://youtu.be/Vdz2TPtCmAk',
+        'https://youtu.be/3_fr5l-JvTM',
+        'https://youtu.be/JYz3tc5NuGY',
+        'https://youtu.be/OvZdr4XM_UE',
+        'https://youtu.be/SDdfIqJLrq4',
+        'https://youtu.be/eEQ9Tq0RRIg',
+        'https://youtu.be/JxOaXPwwWwo',
     ];
 
     public function loadData(ObjectManager $manager)
     {
         $this->createMany(30, 'video', function ($i) {
-            $videoUrl = $this->faker->randomElement(self::$url);
+            $videoUrl = self::$url[$i];
 
             preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $videoUrl, $match);
 
