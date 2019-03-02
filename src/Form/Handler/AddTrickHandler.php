7f164252e7579cc99f3bc082c6218965c17a10ab
tcheuD <tcheutcheud@gmail.com>
2019-03-02 19:05:47 +0100

    addTrickHandler use SlugBuilder VideoIdExtractor to set slug and video identif

--- src/Form/Handler/AddTrickHandler.php
+++ src/Form/Handler/AddTrickHandler.php
@@ -3,28 +3,34 @@
 namespace App\Form\Handler;
 
 use App\Entity\Trick;
+use App\Service\SlugBuilder;
+use App\Service\VideoIdExtractor;
 use Symfony\Component\Form\FormInterface;
 
 class AddTrickHandler
 {
+
     public function handle(FormInterface $form, Trick $trick, $user)
     {
         if ($form->isSubmitted() && $form->isValid()) {
 
+            $videoIdExtractor = new VideoIdExtractor();
+            $slugBuilder = new SlugBuilder();
+
             $trick = $form->getData();
 
-            $trick->setSlug(strtolower(str_replace(' ', '-', $trick->getTitle())));
+            $trick->setSlug($slugBuilder->buildSlug($trick->getTitle()));
             $trick->setCreatedBy($user);
 
 
             $videosCollection = $form->getData()->getVideos()->toArray();
             foreach ($videosCollection as $b => $video) {
-                preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $video->getUrl(), $match);
+
                 $videos[] = $video->getUrl();
                 $video->setNumber(1);
                 $video->setTrick($trick);
                 $video->setCreationDate(new \DateTime());
-                $video->setIdentif($match[1]);
+                $video->setIdentif($videoIdExtractor->urlToId($video->getUrl()));
                 $video->setAuthor($trick->getCreatedBy());
             }
 
