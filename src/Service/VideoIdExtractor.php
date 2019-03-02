7e4cc0a72129f9deaae55070bd1c1fa6900b8435
tcheuD <tcheutcheud@gmail.com>
2019-03-02 18:43:16 +0100

    Added VideoIdExtractor

---
+++ src/Service/VideoIdExtractor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service;
+
+class VideoIdExtractor
+{
+    public function urlToId($url)
+    {
+
+        $pregMatch = preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i',
+            $url, $match);
+
+        if ($pregMatch)
+        {
+            return $match[1];
+        }
+
+        return;
+    }
+
+}
