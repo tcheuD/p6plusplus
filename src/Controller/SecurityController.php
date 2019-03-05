dc57ec03a51736c46b733fa09067dafe3b05198b
tcheuD <tcheutcheud@gmail.com>
2019-03-05 15:07:34 +0100

    Added register method

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\Handler\UserRegistrationHandler;
+use App\Form\UserRegistrationFormType;
+use App\Service\FileUploader;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+class SecurityController extends BaseController
 {
     /**
      * @Route("/login", name="app_login")
@@ -30,5 +35,34 @@ class SecurityController extends AbstractController
     public function logout()
     {
 
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, FileUploader $fileUploader)
+    {
+
+        $user = new User();
+
+        $form = $this->createForm(UserRegistrationFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        $userRegistrationHandler = new UserRegistrationHandler();
+
+        if ($userRegistrationHandler->handle($form, $user, $passwordEncoder, $fileUploader))
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_homepage');
+
+        }
+
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView()
+        ]);
     }
 }
