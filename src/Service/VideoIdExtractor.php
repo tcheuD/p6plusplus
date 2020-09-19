44eb7fe5b81df29d6579b5cd8648be6475b3c830
tcheuD <tcheutcheud@gmail.com>
2020-09-19 12:01:59 +0200

    refactor(urlToId)

--- src/Service/VideoIdExtractor.php
+++ src/Service/VideoIdExtractor.php
@@ -4,18 +4,16 @@ namespace App\Service;
 
 class VideoIdExtractor
 {
-    public function urlToId($url)
+    /**
+     * @param string $url
+     * @return false|string
+     */
+    public function urlToId(string $url)
     {
-
         $pregMatch = preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i',
             $url, $match);
 
-        if ($pregMatch)
-        {
-            return $match[1];
-        }
-
-        return false;
+        return $pregMatch ? $match[1] : false;
     }
 
 }
